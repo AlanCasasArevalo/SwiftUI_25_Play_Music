e720bb5fa5cc352f87b6d30d8cbc002fa6cda179
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-08 06:44:22 +0200

    [FEATURE][BUTTONS] UI buttons were added

--- SwiftUI_25_Play_Music/ContentView.swift
+++ SwiftUI_25_Play_Music/ContentView.swift
@@ -1,14 +1,62 @@
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Hello, World!")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .foregroundColor(.white)
+                    .bold()
+                HStack {
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "backward.fill")
+                            .modifier(CustomButtonModifier())
+                    }
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "play.fill")
+                            .modifier(CustomButtonModifier())
+                    }
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "pause.fill")
+                            .modifier(CustomButtonModifier())
+                    }
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Image(systemName: "forward.fill")
+                            .modifier(CustomButtonModifier())
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(Color(.white))
+            .font(.system(.largeTitle, design: .rounded))
+            .padding()
     }
 }
